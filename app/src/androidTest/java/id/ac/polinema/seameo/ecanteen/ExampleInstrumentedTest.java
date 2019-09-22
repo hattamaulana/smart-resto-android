91500bae02ab2caaffe7f1eadae024b3b62715af
HattaMaulana <mahatta.maulana@gmail.com>
2019-09-22 23:57:59 +0700

    fix error aapt2

--- app/src/androidTest/java/id/ac/polinema/seameo/ecanteen/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/polinema/seameo/ecanteen/ExampleInstrumentedTest.java
@@ -1,13 +1,23 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * Author: Mahatta Maulana
+ * Github: https://github.com/hattamaulana
+ *
+ * Last Modified at 9/19/19 9:15 PM
+ */
+
 package id.ac.polinema.seameo.ecanteen;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
--- app/src/test/java/id/ac/polinema/seameo/ecanteen/ExampleUnitTest.java
+++ app/src/test/java/id/ac/polinema/seameo/ecanteen/ExampleUnitTest.java
@@ -1,8 +1,17 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * Author: Mahatta Maulana
+ * Github: https://github.com/hattamaulana
+ *
+ * Last Modified at 9/19/19 9:15 PM
+ */
+
 package id.ac.polinema.seameo.ecanteen;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
