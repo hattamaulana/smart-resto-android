8018fca33f8d67b0cb0b4fdd91821338065f8cca
hattamaulana <mahatta.maulana@gmail.com>
2021-01-22 13:40:58 +0700

    update && upgrade

--- app/src/main/java/id/ac/polinema/seameo/ecanteen/model/CallingWaiterModel.kt
+++ app/src/main/java/id/ac/polinema/seameo/ecanteen/model/CallingWaiterModel.kt
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2019.
+ * Copyright (c) 2021.
  *
  * Author: Mahatta Maulana
  * Github: https://github.com/hattamaulana
  *
- * Last Modified at 9/27/19 10:57 PM
+ * Last Modified at 1/22/21 11:52 AM
  */
 
 package id.ac.polinema.seameo.ecanteen.model
 
-data class CallingWaiterModel (var uid: String = "", var noTable: Int = -1, var note: String = "") {
+data class CallingWaiterModel(var uid: String = "", var noTable: Int = -1, var note: String = "") {
     fun toMap(): Map<String, Any?> = mapOf(
             "uid" to uid,
             "noTable" to noTable,
